bf70e4c6e0ed1060558a7cbc04b71d73f6936c7e
lukabuz <luka.buzaladze@mail.utoronto.ca>
2018-11-02 01:28:56 -0500

    change to async logic
    -database.js is now a promise based module.
    -created createDataset() method in database.js module. this method can be used to submit a new dataset for training. these datasets first have to be verified to be used for training.
    -created verifyDataset() method in database.js module. This method can be used to verify a dataset and train the bot on it.

--- index.js
+++ index.js
@@ -3,27 +3,65 @@ var bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const db = require('./database.js')
+const { performance } = require('perf_hooks');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let random = db.getRandomWord().then(random => {console.log('random word' , random)});
-let list = db.getNextWords('test').then(res => {console.log('list' , res)});
+chooseNextWord = (nextWords) => {
+    let randomWeight = Math.random(0, 1);
 
-app.get('/', (req, res) => {
+    for(let j = 0; j < nextWords.length; j++){
+        randomWeight = randomWeight - nextWords[j].chance;
+        if(randomWeight <= 0) { 
+            console.log('randomWeight: ', randomWeight);
+            return nextWords[j].nextWord;
+        }
+    }
+}
+
+app.get('/', async (req, res)=> {
     if(!req.query.length){ res.json({ status: 'error', error: 'Please provide the sentence length.' }) }
+    let length = req.query.length;
+    let t0 = performance.now();
+    
+    let previousWord = await db.getRandomWord();
+    let sentence = '';
 
-    res.json({ status: 'success', message: lastWord })
+    for(i = 0; i < length; i++){
+        console.log('___________');
+        console.log('i: ', i);
+        console.log('previousword: ', previousWord);
+        sentence = sentence + previousWord;
+        if(Math.random() <= 0.05) { sentence = sentence + '. ' } else { sentence = sentence + ' ' }
+        
+        let nextWords = await db.getNextWords(previousWord);
+
+        console.log('nextWords: ', nextWords);
+
+        previousWord = chooseNextWord(nextWords);
+
+        console.log('sentence: ', sentence);
+        console.log('___________');
+    }
+
+    let time = (performance.now() - t0) / 1000;
+    time = Number((time).toFixed(1)) + ' s';
+
+    res.json({ status: 'success', message: sentence, time: time })
 });
 
-app.post('/train', (req, res) => {
-    if(!req.query.text){ res.json({ status: 'error', error: 'Please provide the response texts.' }) }
-    let text = req.query.text;
+app.post('/train', async (req, res) => {
+    if(!req.body.text){ res.json({ status: 'error', error: 'Please provide the response texts.' }) }
+    let text = req.body.text;
     text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g," ").replace(/\s{2,}/g," ");;
     let wordsArray = text.split(' ');
 
     for(let i = 0; i < wordsArray.length - 1; i++){
-        db.addCombination(wordsArray[i], wordsArray[i + 1]);
+        await db.addCombination(wordsArray[i], wordsArray[i + 1]);
     }
+
+    console.log('done adding ' + wordsArray.length + ' words to Database')
 
     res.json({ status: 'success', message: 'Text has been parsed and bot has been trained.' })
 });

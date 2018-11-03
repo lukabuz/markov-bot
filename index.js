d796bac83a4b58c8ff4ae0cc8e16039fae97a6cd
lukabuz <luka.buzaladze@mail.utoronto.ca>
2018-11-03 02:59:54 -0500

    added CORS support

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 chooseNextWord = (nextWords) => {
     let randomWeight = Math.random(0, 1);
 

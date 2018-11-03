2cf9d2b33b8bc07c5f844f543f031599275f695c
lukabuz <luka.buzaladze@mail.utoronto.ca>
2018-11-02 20:30:22 -0500

    port change for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 const db = require('./database.js')
 const validator = require('./validator.js')
 const { performance } = require('perf_hooks');
